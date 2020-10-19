b2aba045c9693f6f507f3858783d59333df53354
Lakret <rakuretu@gmail.com>
2020-10-19 23:56:25 +0200

    FIXME note

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-extern crate rspotify;
-
 use music_box::authenticate_spotify;
 use music_box::Result;
 
 fn main() -> Result<()> {
   let spotify = authenticate_spotify()?;
 
+  // FIXME: That might fail with `Compat { error: Other(404) }`;
+  // Please, trigger playback on your desired device manually
+  // to set the default device, and try again.
   spotify.start_playback(None, None, None, None, None)?;
   std::thread::sleep(std::time::Duration::from_secs(5));
   spotify.pause_playback(None)?;
