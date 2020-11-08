33a9d33cb6e28a14cd712629dff9d7330c1ba607
Lakret <Rakuretu@gmail.com>
2020-11-08 19:43:40 +0100

    react & babel setup

--- dev.sh
+++ dev.sh
@@ -1,4 +1,5 @@
 # RUST_LOG=info cargo watch -s 'cargo run --bin webserver' & \
 RUST_LOG=info cargo run --bin webserver & \
+  npm --prefix webserver/assets install webserver/assets & \
   parcel watch --out-dir webserver/assets/dist  webserver/assets/index.html
 # & cargo run --bin desktop
--- webserver/assets/index.js
+++ webserver/assets/index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+import React from "react";
+import ReactDOM from "react-dom";
+
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+var mountNode = document.getElementById("root");
+ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
